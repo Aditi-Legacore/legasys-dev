66cd2559e3de250c14b04cb5cb82f0f4fd272903
Satya <satya.pattanaik145@gmail.com>
2025-10-15 10:29:01 +0530

    -

--- src/components/table/IntakeTable.tsx
+++ src/components/table/IntakeTable.tsx
@@ -314,7 +314,7 @@ export default function CaseIntakeManagement() {
 
         {/* View Modal */}
         {showModal && selectedIntake && (
-          <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4 z-50">
+          <div className="fixed inset-0 bg-transparent bg-opacity-50 flex items-center justify-center p-4 z-50">
             <div className="bg-white dark:bg-gray-800 rounded-lg shadow-xl max-w-md w-full p-6 relative">
               <button
                 onClick={() => setShowModal(false)}
