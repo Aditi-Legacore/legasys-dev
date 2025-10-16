eb524e3797d9305424e9d1014f95a16876e19b2d
Aditi <aditi.legacore@gmail.com>
2025-10-16 09:32:28 +0530

    resolve auth images

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -12,7 +12,9 @@ import { Form, FormField, FormItem, FormControl, FormMessage } from "@/component
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Mail, Lock, Eye, EyeOff, Loader2 } from "lucide-react";
-
+import LoginBg from "../../../public/assets/images/auth/login-bg.png";
+import LoginLogoBg from "../../../public/assets/images/auth/logo.png";
+import LoginGoogleBg from "../../../public/assets/images/auth/google-icon.webp";
 interface LoginFormData {
   email: string;
   password: string;
@@ -65,12 +67,12 @@ export default function LoginForm() {
   };
 
   return (
-    <div className="min-h-screen flex flex-col md:flex-row bg-white">
+    <div className="min-h-screen flex flex-col justify-center md:flex-row bg-white">
       {/* Left side: Illustration */}
-      <div className="hidden md:flex md:w-1/2 relative items-center justify-center bg-gradient-to-br">
-        <div className="relative w-full h-[90vh]"> {/* adjust height as needed */}
+      <div className="hidden lg:flex lg:w-1/2 relative items-center justify-center bg-gradient-to-br">
+        <div className="relative w-full h-[70vh] lg:h-[80vh] xl:h-[90vh]"> {/* adjust height as needed */}
           <Image
-            src="/login-bg.webp"
+            src={LoginBg}
             alt="Login illustration"
             fill
             className="object-cover"
@@ -81,16 +83,16 @@ export default function LoginForm() {
 
 
       {/* Right side: Form */}
-      <div className="flex-1 md:w-1/2 flex items-center justify-center p-8">
+      <div className="lg:w-1/2 flex items-center justify-center p-8">
         <div className="w-full max-w-md space-y-8">
           {/* Logo and Title */}
-          <div className="space-y-2">
-            <div className="flex items-center gap-2 mb-6">
-               <div className="w-10 h-10 bg-white rounded-lg flex items-center justify-center overflow-hidden">
+          <div className="space-y-2 text-center">
+            <div className="flex items-center justify-center gap-2 mb-6">
+              <div className="w-10 h-10 md:w-[50px] md:h-[50px] bg-white rounded-lg flex items-center justify-center overflow-hidden">
                 <Image
-                  src="/legacore.png"
+                  src={LoginLogoBg}
                   alt="Logo"
-                  width={50} 
+                  width={50}
                   height={50}
                   className="object-contain"
                   priority
@@ -205,7 +207,7 @@ export default function LoginForm() {
                   onClick={() => signIn("google", { callbackUrl: "/" })}
                 >
                   <Image
-                    src="/google-icon.webp"
+                    src={LoginGoogleBg}
                     alt="Google logo"
                     width={20}
                     height={20}
