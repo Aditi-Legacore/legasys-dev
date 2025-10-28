2127cd7179a3fbc86d309125a4ef1f41b631da3f
Rajalaxmi <rajalaxmi7787@gmail.com>
2025-10-28 15:25:24 +0530

    changes in medical info

--- src/components/forms/formSteps/MedicalTreatmentStep.tsx
+++ src/components/forms/formSteps/MedicalTreatmentStep.tsx
@@ -40,47 +40,6 @@ export default function MedicalTreatmentStep() {
         <InputField name="lengthOfStay" label="Length of stay" />
       )}
 
-      {/* 3. Prior Injuries Section */}
-      <RadioGroup
-        name="priorInjuries"
-        label="Any Prior Injuries? *"
-        options={["Yes", "No"]}
-      />
-      {priorInjuries === "Yes" && (
-        <div className="p-4 rounded-2xl border border-gray-300 dark:border-gray-700">
-          <h3 className="font-semibold text-lg mb-2 text-gray-700 dark:text-gray-200">
-            Prior Injuries Details
-          </h3>
-          <InputField name="priorDoctorHospital" label="Doctor/Hospital Name" />
-          <InputField
-            name="priorHospitalAddressPhone"
-            label="Hospital Address & Phone No."
-          />
-          <TextareaField
-            name="priorTreatmentDetails"
-            label="Treatment Details"
-          />
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-            <DateInputField
-              name="priorTreatmentFrom"
-              label="Treatment Date (From)"
-            />
-            <DateInputField
-              name="priorTreatmentTo"
-              label="Treatment Date (To)"
-            />
-          </div>
-          <InputField
-            name="priorInsuranceClaims"
-            label="Prior Insurance Claim #"
-          />
-          <InputField
-            name="priorAttorneys"
-            label="Prior Attorneys for PI / WC Injuries"
-          />
-        </div>
-      )}
-
       {/* 4. Current Treatment Section */}
       <RadioGroup
         name="currentTreatment"
@@ -122,6 +81,48 @@ export default function MedicalTreatmentStep() {
         name="bodyPartsAffected"
         label="Body Parts Affected"
       />
+
+       {/* 3. Prior Injuries Section */}
+      <RadioGroup
+        name="priorInjuries"
+        label="Any Prior Injuries? *"
+        options={["Yes", "No"]}
+      />
+      {priorInjuries === "Yes" && (
+        <div className="p-4 rounded-2xl border border-gray-300 dark:border-gray-700">
+          <h3 className="font-semibold text-lg mb-2 text-gray-700 dark:text-gray-200">
+            Prior Injuries Details
+          </h3>
+          <InputField name="priorDoctorHospital" label="Doctor/Hospital Name" />
+          <InputField
+            name="priorHospitalAddressPhone"
+            label="Hospital Address & Phone No."
+          />
+          <TextareaField
+            name="priorTreatmentDetails"
+            label="Treatment Details"
+          />
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+            <DateInputField
+              name="priorTreatmentFrom"
+              label="Treatment Date (From)"
+            />
+            <DateInputField
+              name="priorTreatmentTo"
+              label="Treatment Date (To)"
+            />
+          </div>
+          <InputField
+            name="priorInsuranceClaims"
+            label="Prior Insurance Claim #"
+          />
+          <InputField
+            name="priorAttorneys"
+            label="Prior Attorneys for PI / WC Injuries"
+          />
+        </div>
+      )}
+      
     </div>
   );
 }
