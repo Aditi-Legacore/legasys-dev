b801943d7c8306aa7727b403e954a7582549508c
Satya <sp5339586@gmail.com>
2025-11-17 11:46:15 +0530

    css fixed

--- src/app/forms/[id]/fill/layout.tsx
+++ src/app/forms/[id]/fill/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "../../globals.css";
+import "../../../globals.css";
 import { Providers } from "../../../../components/ThemeProvider";
 import { Inter } from "next/font/google";
 
